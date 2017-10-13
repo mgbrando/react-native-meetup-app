55a20ded9850016d76bc2f9a5c528b08bb5b282c
mgbrando <mgbrando29@gmail.com>
2017-10-13 03:17:20 -0400

    Initial settings for a React Native App

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// const add = (a: string, b: string) => a + b;
+
+// add('hi', 'fly');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function app() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+    </View>
+  );
+}
+/* export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+} */
